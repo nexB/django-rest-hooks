5c71567131784fd408503303666809d627e03083
Thomas Druez <tdruez@nexb.com>
2023-04-12 12:58:44 +0400

    Add README about this fork

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setup(
     package_data={
         'rest_hooks': [
             'migrations/*.py',
-            'south_migrations/*.py'
         ]
     },
     classifiers = [
@@ -29,8 +28,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
         'Topic :: Utilities',
     ],
